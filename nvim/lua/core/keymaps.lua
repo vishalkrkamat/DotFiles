f40cdacf1f11680ed2590a7946b6ecb777b7413c
vishalkrkamat <vishalkrkamat7@gmail.com>
2024-12-01 17:14:12 +0530

    keybinds added

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -7,23 +7,18 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
 --
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-
+vim.keymap.set("n", "zz", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<Tab><Tab>", ":wq<CR>", { desc = "Save and quit" })
+vim.keymap.set("n", "<Tab>q", ":q!<CR>", { desc = "Quit!" })
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
+
+vim.keymap.set("t", "<C-u>", [[<C-\><C-n>:w<CR>:!gcc % -o %:r && ./%:r<CR>]], { noremap = true, silent = true })
+-- Map Ctrl + u in terminal mode
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -43,3 +38,9 @@ end
 
 vim.keymap.set("n", "vs", toggle_vsplit, { noremap = true, silent = true })
 --vim.keymap.set("n", "vs", "<cmd>vsplit<CR>", { noremap = true, silent = true })
+--
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
